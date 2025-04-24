3bf1301e577f7dca8d437c216d8c48be37733521
Gabrax <gab-oz@wp.pl>
2025-04-24 23:12:06 +0200

    losing my mind

--- Stereo-Vision/stereo.py
+++ Stereo-Vision/stereo.py
@@ -1,6 +1,7 @@
 ﻿import numpy as np
 import cv2
-from openpyxl import Workbook # Used for writing data into an Excel file
+import time
+# from openpyxl import Workbook # Used for writing data into an Excel file
 from sklearn.preprocessing import normalize
 
 # Filtering
@@ -18,6 +19,10 @@ def coords_mouse_disp(event,x,y,flags,param):
         Distance= np.around(Distance*0.01,decimals=2)
         print('Distance: '+ str(Distance)+' m')
 
+def estimate_distance(disparity_value):
+    distance = -593.97 * disparity_value**3 + 1506.8 * disparity_value**2 - 1373.1 * disparity_value + 522.06
+    return np.around(distance * 0.01, decimals=2)  # Convert to meters
+
 # Termination criteria
 criteria =(cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 criteria_stereo= (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
@@ -31,76 +36,43 @@ objpoints= []   # 3d points in real world space
 imgpointsR= []   # 2d points in image plane
 imgpointsL= []
 
-# Start calibration from the camera
 print('Starting stereo calibration ... ')
 
-for i in range(0,1):   # Put the amount of pictures you have taken for the calibration inbetween range(0,?) wenn starting from the image number 0
-    t= str(i)
-    ChessImaR= cv2.imread('calib_images/right_chessboard-'+t+'.png',0)    # Right side
-    ChessImaL= cv2.imread('calib_images/left_chessboard-'+t+'.png',0)    # Left side
-    retR, cornersR = cv2.findChessboardCorners(ChessImaR,
-                                               (9,6),None)  # Define the number of chees corners we are looking for
-    retL, cornersL = cv2.findChessboardCorners(ChessImaL,
-                                               (9,6),None)  # Left side
-    if (True == retR) & (True == retL):
+for i in range(0, 64):
+    t = str(i)
+    ChessImaR = cv2.imread('calib_images/right_chessboard-' + t + '.png', 0)
+    ChessImaL = cv2.imread('calib_images/left_chessboard-' + t + '.png', 0)
+    retR, cornersR = cv2.findChessboardCorners(ChessImaR, (9, 6), None)
+    retL, cornersL = cv2.findChessboardCorners(ChessImaL, (9, 6), None)
+    if retR and retL:
         objpoints.append(objp)
-        cv2.cornerSubPix(ChessImaR,cornersR,(11,11),(-1,-1),criteria)
-        cv2.cornerSubPix(ChessImaL,cornersL,(11,11),(-1,-1),criteria)
+        cv2.cornerSubPix(ChessImaR, cornersR, (11, 11), (-1, -1), criteria)
+        cv2.cornerSubPix(ChessImaL, cornersL, (11, 11), (-1, -1), criteria)
         imgpointsR.append(cornersR)
         imgpointsL.append(cornersL)
 
-# Determine the new values for different parameters
-#   Right Side
-retR, mtxR, distR, rvecsR, tvecsR = cv2.calibrateCamera(objpoints,
-                                                        imgpointsR,
-                                                        ChessImaR.shape[::-1],None,None)
-
-hR,wR= ChessImaR.shape[:2]
-OmtxR, roiR= cv2.getOptimalNewCameraMatrix(mtxR,distR,
-                                                   (wR,hR),1,(wR,hR))
-
-#   Left Side
-retL, mtxL, distL, rvecsL, tvecsL = cv2.calibrateCamera(objpoints,
-                                                        imgpointsL,
-                                                        ChessImaL.shape[::-1],None,None)
-hL,wL= ChessImaL.shape[:2]
-OmtxL, roiL= cv2.getOptimalNewCameraMatrix(mtxL,distL,(wL,hL),1,(wL,hL))
+# Calibration
+retR, mtxR, distR, rvecsR, tvecsR = cv2.calibrateCamera(objpoints, imgpointsR, ChessImaR.shape[::-1], None, None)
+retL, mtxL, distL, rvecsL, tvecsL = cv2.calibrateCamera(objpoints, imgpointsL, ChessImaL.shape[::-1], None, None)
+OmtxR, roiR = cv2.getOptimalNewCameraMatrix(mtxR, distR, ChessImaR.shape[::-1], 1, ChessImaR.shape[::-1])
+OmtxL, roiL = cv2.getOptimalNewCameraMatrix(mtxL, distL, ChessImaL.shape[::-1], 1, ChessImaL.shape[::-1])
 
 print('Cameras Ready to use')
 
-retS, MLS, dLS, MRS, dRS, R, T, E, F= cv2.stereoCalibrate(objpoints,
-                                                          imgpointsL,
-                                                          imgpointsR,
-                                                          mtxL,
-                                                          distL,
-                                                          mtxR,
-                                                          distR,
-                                                          ChessImaR.shape[::-1],
-                                                          criteria = criteria_stereo,
-                                                          flags = cv2.CALIB_FIX_INTRINSIC)
+retS, MLS, dLS, MRS, dRS, R, T, E, F= cv2.stereoCalibrate(objpoints,imgpointsL,imgpointsR,mtxL,distL,mtxR,distR,ChessImaR.shape[::-1],criteria = criteria_stereo,flags = cv2.CALIB_FIX_INTRINSIC)
 
 # StereoRectify function
 rectify_scale= 0 # if 0 image croped, if 1 image nor croped
-RL, RR, PL, PR, Q, roiL, roiR= cv2.stereoRectify(MLS, dLS, MRS, dRS,
-                                                 ChessImaR.shape[::-1], R, T,
-                                                 rectify_scale,(0,0))  # last paramater is alpha, if 0= croped, if 1= not croped
+RL, RR, PL, PR, Q, roiL, roiR= cv2.stereoRectify(MLS, dLS, MRS, dRS, ChessImaR.shape[::-1], R, T, rectify_scale,(0,0))  # last paramater is alpha, if 0= croped, if 1= not croped
 # initUndistortRectifyMap function
-Left_Stereo_Map= cv2.initUndistortRectifyMap(MLS, dLS, RL, PL,
-                                             ChessImaR.shape[::-1], cv2.CV_16SC2)   # cv2.CV_16SC2 this format enables us the programme to work faster
-Right_Stereo_Map= cv2.initUndistortRectifyMap(MRS, dRS, RR, PR,
-                                              ChessImaR.shape[::-1], cv2.CV_16SC2)
+Left_Stereo_Map= cv2.initUndistortRectifyMap(MLS, dLS, RL, PL, ChessImaR.shape[::-1], cv2.CV_16SC2)   # cv2.CV_16SC2 this format enables us the programme to work faster
+Right_Stereo_Map= cv2.initUndistortRectifyMap(MRS, dRS, RR, PR, ChessImaR.shape[::-1], cv2.CV_16SC2)
 
 # Create StereoSGBM and prepare all parameters
 window_size = 3
 min_disp = 2
 num_disp = 130-min_disp
-stereo = cv2.StereoSGBM_create(minDisparity = min_disp,
-    numDisparities = num_disp,
-    blockSize = window_size,
-    uniquenessRatio = 10,
-    speckleWindowSize = 100,
-    speckleRange = 32,
-    disp12MaxDiff = 5,
+stereo = cv2.StereoSGBM_create(minDisparity = min_disp,numDisparities = num_disp,blockSize = window_size,uniquenessRatio = 10,speckleWindowSize = 100,speckleRange = 32, disp12MaxDiff = 5,
     P1 = 8*3*window_size**2,
     P2 = 32*3*window_size**2)
 
@@ -111,21 +83,20 @@ stereoR=cv2.ximgproc.createRightMatcher(stereo) # Create another stereo for righ
 lmbda = 80000
 sigma = 1.8
 visual_multiplier = 1.0
- 
 wls_filter = cv2.ximgproc.createDisparityWLSFilter(matcher_left=stereo)
 wls_filter.setLambda(lmbda)
 wls_filter.setSigmaColor(sigma)
 
 Cam = cv2.VideoCapture(0)
-
 Cam.set(cv2.CAP_PROP_FRAME_WIDTH, 960)  
 Cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 270)
 Cam.set(cv2.CAP_PROP_FPS, 60)
 
-while True:
-    # Start Reading Camera images
-    ret, frame= Cam.read()
+prev_time = time.time()
 
+while True:
+    current_time = time.time()
+    ret, frame= Cam.read()
     if not ret:
         print("❌ Failed to capture frame. Exiting...")
         break
@@ -137,7 +108,7 @@ while True:
     right_frame = frame[:, mid:]  # Right camera view
 
 
-    Left_nice= cv2.remap(left_frame,Left_Stereo_Map[0],Left_Stereo_Map[1], interpolation = cv2.INTER_LANCZOS4, borderMode = cv2.BORDER_CONSTANT)  # Rectify the image using the kalibration parameters founds during the initialisation
+    Left_nice= cv2.remap(left_frame,Left_Stereo_Map[0],Left_Stereo_Map[1], interpolation = cv2.INTER_LANCZOS4, borderMode = cv2.BORDER_CONSTANT)  
     Right_nice= cv2.remap(right_frame,Right_Stereo_Map[0],Right_Stereo_Map[1], interpolation = cv2.INTER_LANCZOS4, borderMode = cv2.BORDER_CONSTANT)
 
     gray_left = cv2.cvtColor(left_frame, cv2.COLOR_BGR2GRAY)
@@ -149,33 +120,57 @@ while True:
     dispL= np.int16(dispL)
     dispR= np.int16(dispR)
 
+    #cv2.imshow('Disparity', disp)
+    # closing= cv2.morphologyEx(disp,cv2.MORPH_CLOSE, kernel) # Apply an morphological filter for closing little "black" holes in the picture(Remove noise) 
+    #cv2.imshow('Closing',closing)
+    # dispc= (closing-closing.min())*255
+    # dispC= dispc.astype(np.uint8)                                   # Convert the type of the matrix from float32 to uint8, this way you can show the results with the function cv2.imshow()
+    # disp_Color= cv2.applyColorMap(dispC,cv2.COLORMAP_OCEAN)         # Change the Color of the Picture into an Ocean Color_Map
+    #cv2.imshow('Color Depth',disp_Color)
+    disp= ((disp.astype(np.float32)/ 16)-min_disp)/num_disp # Calculation allowing us to have 0 for the most distant object able to detect
+    # roi_mask = np.zeros_like(gray_left, dtype=np.uint8)
+    # cv2.rectangle(roi_mask, (100, 100), (300, 300), 255, -1)  # Only process this region
     filteredImg= wls_filter.filter(dispL,gray_left,None,dispR)
     filteredImg = cv2.normalize(src=filteredImg, dst=filteredImg, beta=0, alpha=255, norm_type=cv2.NORM_MINMAX);
     filteredImg = np.uint8(filteredImg)
     #cv2.imshow('Disparity Map', filteredImg)
-    disp= ((disp.astype(np.float32)/ 16)-min_disp)/num_disp # Calculation allowing us to have 0 for the most distant object able to detect
 
-    closing= cv2.morphologyEx(disp,cv2.MORPH_CLOSE, kernel) # Apply an morphological filter for closing little "black" holes in the picture(Remove noise) 
+    filt_Color= cv2.applyColorMap(filteredImg,cv2.COLORMAP_OCEAN)
 
-    # Colors map
-    dispc= (closing-closing.min())*255
-    dispC= dispc.astype(np.uint8)                                   # Convert the type of the matrix from float32 to uint8, this way you can show the results with the function cv2.imshow()
-    disp_Color= cv2.applyColorMap(dispC,cv2.COLORMAP_OCEAN)         # Change the Color of the Picture into an Ocean Color_Map
-    filt_Color= cv2.applyColorMap(filteredImg,cv2.COLORMAP_OCEAN) 
+    # --- Close Object Detection ---
+    _, close_mask = cv2.threshold(filteredImg, 160, 255, cv2.THRESH_BINARY)
+    close_mask = cv2.morphologyEx(close_mask, cv2.MORPH_CLOSE, kernel)
+    contours, _ = cv2.findContours(close_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    # Show the result for the Depth_image
-    #cv2.imshow('Disparity', disp)
-    #cv2.imshow('Closing',closing)
-    #cv2.imshow('Color Depth',disp_Color)
-    cv2.imshow('Filtered Color Depth',filt_Color)
+    for cnt in contours:
+        if cv2.contourArea(cnt) > 500:
+            x, y, w, h = cv2.boundingRect(cnt)
+            cv2.rectangle(filt_Color, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-    # Mouse click
-    cv2.setMouseCallback("Filtered Color Depth",coords_mouse_disp,filt_Color)
+            # Use RAW disparity map (just like in coords_mouse_disp)
+            roi_disp = disp[y:y + h, x:x + w].astype(np.float32)
+
+            # Take center 3x3 window for accurate comparison
+            cx, cy = x + w // 2, y + h // 2
+            sample_disp = disp[cy - 1:cy + 2, cx - 1:cx + 2].astype(np.float32)
+            average_disp = np.mean(sample_disp[sample_disp > 0])
+
+            if average_disp > 0:
+                distance = -593.97 * average_disp**3 + 1506.8 * average_disp**2 - 1373.1 * average_disp + 522.06
+                distance = np.around(distance * 0.01, decimals=2)
+                cv2.putText(filt_Color, f"{distance:.2f} m", (x, y - 10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    # --------------------------------
     
-    # End the Programme
-    if cv2.waitKey(1) & 0xFF == ord(' '):
+    fps = 1 / (current_time - prev_time)
+    prev_time = current_time
+
+    cv2.putText(filt_Color, f"FPS: {fps:.2f}", (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2)
+    cv2.imshow('Filtered Color Depth', filt_Color)
+    cv2.setMouseCallback("Filtered Color Depth",coords_mouse_disp,filt_Color)
+
+    if cv2.waitKey(1) & 0xFF == 27:
         break
 
 Cam.release()
 cv2.destroyAllWindows()
-
